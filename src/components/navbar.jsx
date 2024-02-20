0dc580970af9647f69940f011329039fd19699ba
MariusPetreF <141928736+MariusPetreF@users.noreply.github.com>
2024-02-20 21:49:12 +0200

    init

---
+++ src/components/navbar.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link"
+
+const links = [
+    { url: "/", title: "Home"},
+    { url: "/about", title: "About"},
+    { url: "/portfolio", title: "Portfolio"},
+    { url: "/contact", title: "Contact"},
+];
+
+const Navbar = () => {
+    return (
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+            {/* LOGO */}
+            <div className="">
+                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
+                    <span className="text-white mr-1">Lama</span>
+                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.dev</span>
+                </Link>
+            </div>
+            {/* RESPONSIVE MENU */}
+            <div className="">
+                {/* MENU BUTTON */}
+                <button className="w-10 h-8 flex flex-col justify-between">
+                    <div className="w-10 h-1 bg-black rounded"></div>
+                    <div className="w-10 h-1 bg-black rounded"></div>
+                    <div className="w-10 h-1 bg-black rounded"></div>
+                </button>
+                {/* MENU LIST */}
+                <div className="absolute top-0 left-0 w-screen h-screen text-white"></div>
+                {links.map(link=>(
+                    <Link href={link.url} key={link.title}>{link.title}</Link>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
