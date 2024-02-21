6a0d6f561a464b838002ccbdb90b798fa9d27ce5
Marius <mayutz96@gmail.com>
2024-02-21 10:36:10 +0200

    init

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { useState } from "react";
 
 const links = [
     { url: "/", title: "Home"},
@@ -8,6 +9,9 @@ const links = [
 ];
 
 const Navbar = () => {
+
+    const[open, setOpen] = useState(false);
+
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
             {/* LOGO */}
@@ -20,16 +24,20 @@ const Navbar = () => {
             {/* RESPONSIVE MENU */}
             <div className="">
                 {/* MENU BUTTON */}
-                <button className="w-10 h-8 flex flex-col justify-between">
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
+                <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
                 </button>
                 {/* MENU LIST */}
-                <div className="absolute top-0 left-0 w-screen h-screen text-white"></div>
+                {open && (
+
+                    <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
                 {links.map(link=>(
                     <Link href={link.url} key={link.title}>{link.title}</Link>
-                ))}
+                    ))}
+                </div>
+                )}
             </div>
         </div>
     )
