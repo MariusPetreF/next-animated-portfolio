8997fbd383a9c8a94d01635d52cc70be5a37993d
MariusPetreF <141928736+MariusPetreF@users.noreply.github.com>
2024-03-14 02:56:21 +0200

    init

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,17 +1,42 @@
-"use client"
+"use client";
 
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 
 const ContactPage = () => {
-    return (
-        <motion.div 
-    className="h-full" 
-    initial={{y:"-200vh"}} 
-    animate={{y:"0%"}}
-    transition={{duration: 1}}>
-        Contact
-    </motion.div>
-    )
-}
+  const text = "Say Hello";
 
-export default ContactPage
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}>
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <div className="">
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+                className="">
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </div>
+        </div>
+        {/* FORM CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2"></div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default ContactPage;
