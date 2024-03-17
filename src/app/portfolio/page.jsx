6b3fc2aad32d303e8140aff1836df4ba3d761a2e
MariusPetreF <141928736+MariusPetreF@users.noreply.github.com>
2024-03-17 13:04:53 +0200

    init

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -54,7 +54,7 @@ const PortfolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}>
       <div className="h-[600vh]" ref={ref}>
-        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+        <div className="w-full h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
           My Works
         </div>
         <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
